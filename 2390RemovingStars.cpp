314a170118edef07d89875d5e9edd7beb07c08ec
Ansal Jain <jainansal@gmail.com>
2023-04-11 19:37:03 +0530

    110423

---
+++ 2390RemovingStars.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string final_string;
+        for(auto current:s)
+        {
+            if(current=='*')
+            {
+                final_string.pop_back();
+            }
+            else
+            {
+                final_string.push_back(current);
+            }
+        }
+        return final_string;
+    }
+};
